b585340cb3bbeb6a7ee999d81bcc304d34f0e978
Soubhik Chakraborty <soubhik560@gmail.com>
2024-01-22 21:46:45 +0530

    exact path update

--- src/App.js
+++ src/App.js
@@ -43,8 +43,8 @@ function App() {
         <div className="container">
 
           <Routes>
-            <Route path='/' element={<Textform heading="Enter Text to Analyze!" mode={mode} showAlert={showAlert} />} />
-            <Route path="/about" element={<About mode={mode} />} />
+            <Route exact path='/' element={<Textform heading="Enter Text to Analyze!" mode={mode} showAlert={showAlert} />} />
+            <Route exact path="/about" element={<About mode={mode} />} />
           </Routes>
         </div>
       </BrowserRouter>
